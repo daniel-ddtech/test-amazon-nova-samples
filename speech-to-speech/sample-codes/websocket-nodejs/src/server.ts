788db5aa5cbca3e4a2805824544eeafdb822b023
daniel-ddtech <161848749+daniel-ddtech@users.noreply.github.com>
2025-04-12 13:43:21 +0300

    server

--- speech-to-speech/sample-codes/websocket-nodejs/src/server.ts
+++ speech-to-speech/sample-codes/websocket-nodejs/src/server.ts
@@ -7,7 +7,7 @@ import { NovaSonicBidirectionalStreamClient } from './client';
 import { Buffer } from 'node:buffer';
 
 // Configure AWS credentials
-const AWS_PROFILE_NAME = process.env.AWS_PROFILE || 'bedrock-test';
+const AWS_PROFILE_NAME = process.env.AWS_PROFILE || 'default';
 
 // Create Express app and HTTP server
 const app = express();
